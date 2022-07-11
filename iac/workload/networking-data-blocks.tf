09000913eae0e983c58d8834af0383c8ba6f8a45
Michael Griehm <griehmmj@gmail.com>
2022-07-11 10:17:49 -0400

    Added back private endpoints back after networking refactor and added self hosted runner

--- iac/workload/networking-data-blocks.tf
+++ iac/workload/networking-data-blocks.tf
@@ -1,15 +1,15 @@
-# data "azurerm_subnet" "storage_private_endpoint" {
-#   name                 = "storage-private-endpoint"
-#   resource_group_name  = "networking-demo-eastus2"
-#   virtual_network_name = "delta-lakehouse-storage-spoke"
-# }
+data "azurerm_subnet" "storage_private_endpoint" {
+  name                 = "storage-private-endpoint"
+  resource_group_name  = "networking-demo-eastus2"
+  virtual_network_name = "delta-lakehouse-spoke"
+}
 
-# data "azurerm_private_dns_zone" "delta_lakehouse_blobs" {
-#   name                = "privatelink.blob.core.windows.net"
-#   resource_group_name = "networking-demo-eastus2"
-# }
+data "azurerm_private_dns_zone" "delta_lakehouse_blobs" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = "networking-demo-eastus2"
+}
 
-# data "azurerm_private_dns_zone" "delta_lakehouse_dfs" {
-#   name                = "privatelink.dfs.core.windows.net"
-#   resource_group_name = "networking-demo-eastus2"
-# }
+data "azurerm_private_dns_zone" "delta_lakehouse_dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = "networking-demo-eastus2"
+}
--- iac/workload/storage-silver.tf
+++ iac/workload/storage-silver.tf
@@ -13,43 +13,43 @@ resource "azurerm_storage_account" "silver" {
   }
 }
 
-# resource "azurerm_private_endpoint" "silver_blob_private_endpoint" {
-#   name                = "dltalakehousesilver-blob-private-endpoint"
-#   location            = data.azurerm_resource_group.rg.location
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   subnet_id           = data.azurerm_subnet.storage_private_endpoint.id
+resource "azurerm_private_endpoint" "silver_blob_private_endpoint" {
+  name                = "dltalakehousesilver-blob-private-endpoint"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.storage_private_endpoint.id
 
-#   private_service_connection {
-#     name                           = "dltalakehousesilver-blob-private-service-connection"
-#     private_connection_resource_id = azurerm_storage_account.bronze.id
-#     is_manual_connection           = false
-#     subresource_names              = ["blob"]
-#   }
+  private_service_connection {
+    name                           = "dltalakehousesilver-blob-private-service-connection"
+    private_connection_resource_id = azurerm_storage_account.bronze.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
 
-#   private_dns_zone_group {
-#     name                 = "privatelink.blob.core.windows.net"
-#     private_dns_zone_ids = [data.azurerm_private_dns_zone.delta_lakehouse_blobs.id]
-#   }
-# }
+  private_dns_zone_group {
+    name                 = "privatelink.blob.core.windows.net"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.delta_lakehouse_blobs.id]
+  }
+}
 
-# resource "azurerm_private_endpoint" "silver_dfs_private_endpoint" {
-#   name                = "dltalakehousesilver-dfs-private-endpoint"
-#   location            = data.azurerm_resource_group.rg.location
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   subnet_id           = data.azurerm_subnet.storage_private_endpoint.id
+resource "azurerm_private_endpoint" "silver_dfs_private_endpoint" {
+  name                = "dltalakehousesilver-dfs-private-endpoint"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.storage_private_endpoint.id
 
-#   private_service_connection {
-#     name                           = "dltalakehousesilver-dfs-private-service-connection"
-#     private_connection_resource_id = azurerm_storage_account.bronze.id
-#     is_manual_connection           = false
-#     subresource_names              = ["dfs"]
-#   }
+  private_service_connection {
+    name                           = "dltalakehousesilver-dfs-private-service-connection"
+    private_connection_resource_id = azurerm_storage_account.bronze.id
+    is_manual_connection           = false
+    subresource_names              = ["dfs"]
+  }
 
-#   private_dns_zone_group {
-#     name                 = "privatelink.dfs.core.windows.net"
-#     private_dns_zone_ids = [data.azurerm_private_dns_zone.delta_lakehouse_dfs.id]
-#   }
-# }
+  private_dns_zone_group {
+    name                 = "privatelink.dfs.core.windows.net"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.delta_lakehouse_dfs.id]
+  }
+}
 
 resource "azurerm_storage_container" "silver_crypto" {
   name                  = "crypto-data"
