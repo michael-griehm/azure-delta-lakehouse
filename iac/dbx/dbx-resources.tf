f8fba90315ccb78464301bb28d5f4a683d2a74f6
Michael Griehm <griehmmj@gmail.com>
2022-07-13 10:30:03 -0400

    Added DBx IaC

---
+++ iac/dbx/dbx-resources.tf
@@ -0,0 +1,56 @@
+data "azurerm_databricks_workspace" "dbx" {
+  name                = "dlta-lakehouse-dbx"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+provider "databricks" {
+  host                        = data.azurerm_databricks_workspace.dbx.workspace_url
+  azure_workspace_resource_id = data.azurerm_databricks_workspace.dbx.id
+  azure_client_id             = data.azurerm_client_config.current.client_id
+  azure_tenant_id             = data.azurerm_client_config.current.tenant_id
+  azure_client_secret         = var.client_secret
+}
+
+data "databricks_spark_version" "latest" {}
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
+
+resource "databricks_user" "dbx_admin" {
+  user_name = data.azuread_user.workload_admin.user_principal_name
+}
+
+resource "databricks_cluster" "experiment" {
+  cluster_name            = "admin-experiment-cluster"
+  spark_version           = data.databricks_spark_version.latest.id
+  node_type_id            = data.databricks_node_type.smallest.id
+  autotermination_minutes = 15
+  single_user_name        = data.azuread_user.workload_admin.user_principal_name
+
+  autoscale {
+    min_workers = 1
+    max_workers = 3
+  }
+
+  spark_conf = {
+    "spark.databricks.passthrough.enabled" : "true"
+  }
+}
+
+data "azurerm_key_vault" "secret_scope_vault" {
+  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "dlta-lkehse-kv"
+}
+
+output "azurerm_databricks_workspace_url" {
+  value = data.azurerm_databricks_workspace.dbx.workspace_url
+}
+
+output "secret_scope_vault_id" {
+  value = data.azurerm_key_vault.secret_scope_vault.id
+}
+
+output "secret_scope_vault_hostname" {
+  value = data.azurerm_key_vault.secret_scope_vault.vault_uri
+}
---
+++ iac/dbx/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "admin_user_principal_name" {
+  type        = string
+  sensitive   = true
+  description = "The user principal name of the admin for the app."
+  default     = "mikeg@ish-star.com"
+}
+
+data "azuread_user" "workload_admin" {
+  user_principal_name = var.admin_user_principal_name
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = "delta-lakehouse-demo-eastus2"
+}
